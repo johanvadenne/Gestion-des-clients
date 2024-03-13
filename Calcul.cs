bd91ca335e247647d42eb6e498bd2516fc522b15
wolffire18 <johanvadenne@gmail.com>
2024-03-13 17:08:57 +0100

    1.1

--- Calcul.cs
+++ Calcul.cs
@@ -13,7 +13,7 @@ namespace Gestion_des_clients
 {
     public partial class Calcul : Form
     {
-        ClasseMetierRideau metierRideau = new ClasseMetierRideau(5.5m, 9.9m, 5.8m, 1, 1);
+        ClasseMetierRideau metierRideau = new ClasseMetierRideau(5.5m, 9.9m, 1.5m, 1, 1);
 
         public Calcul()
         {
@@ -22,9 +22,9 @@ namespace Gestion_des_clients
 
         private void Calcul_Load(object sender, EventArgs e)
         {
-
             decimal total = metierRideau.CoutTotal();
             MessageBox.Show(total.ToString());
+            this.Close();
         }
     }
 }
